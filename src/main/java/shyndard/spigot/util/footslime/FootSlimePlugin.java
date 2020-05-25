df9edc12b352f71988b100cf7e9f6372fd712790
BuildTools <shyndard@gmail.com>
2020-05-25 10:57:00 +0200

    :tada: Initiate project "footslime"

---
+++ src/main/java/shyndard/spigot/util/footslime/FootSlimePlugin.java
@@ -0,0 +1,17 @@
+package shyndard.spigot.util.footslime;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class FootSlimePlugin extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+
+	}
+
+	@Override
+	public void onDisable() {
+
+	}
+	
+}
