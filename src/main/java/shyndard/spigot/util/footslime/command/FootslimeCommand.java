bcb0e90add1b6215487ab71b38ab38029ba1823c
BuildTools <shyndard@gmail.com>
2020-05-25 12:18:47 +0200

    :bug: Fix command block sender verification

--- src/main/java/shyndard/spigot/util/footslime/command/FootslimeCommand.java
+++ src/main/java/shyndard/spigot/util/footslime/command/FootslimeCommand.java
@@ -1,18 +1,22 @@
 package shyndard.spigot.util.footslime.command;
 
 import org.bukkit.block.CommandBlock;
+import org.bukkit.command.BlockCommandSender;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-
+import net.md_5.bungee.api.ChatColor;
 import shyndard.spigot.util.footslime.API;
 
 public class FootslimeCommand implements CommandExecutor {
 
+	private static final String permissionDenied = "Only a command block can do that.";
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		System.out.println(sender.getClass());
-		if((sender instanceof CommandBlock)) return false;
+		if(!(sender instanceof BlockCommandSender)) {
+			sender.sendMessage(ChatColor.RED + permissionDenied);
+			return true;
+		}
 		if(args.length >= 2 && "score".equalsIgnoreCase(args[0])) {
 			if("blue".equalsIgnoreCase(args[1]) || "red".equalsIgnoreCase(args[1])) {
 				API.getInstance().score(args[1]);
